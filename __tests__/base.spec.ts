747164e12ea61feb04fc0eae1675f1acf4164855
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-07 16:04:37 +0800

    Uncomment tests

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -62,62 +62,62 @@ describe("Categories", () => {
     // });
 });
 
-// describe("Channels", () => {
-//     test("Get Channel", async () => {
-//         const channel = await Trovo.channels.get("HerouTV");
-//         expect(channel).toBeTruthy();
-//     });
+describe("Channels", () => {
+    // test("Get Channel", async () => {
+    //     const channel = await Trovo.channels.get("HerouTV");
+    //     expect(channel).toBeTruthy();
+    // });
 
-//     test("Top Channels", async () => {
-//         const top = await Trovo.channels.top();
-//         expect(top).toBeTruthy();
-//     });
-// });
+    // test("Top Channels", async () => {
+    //     const top = await Trovo.channels.top();
+    //     expect(top).toBeTruthy();
+    // });
+});
 
-// describe("Channel", () => {
-//     test("Edit Channel", async () => {
-//         const channel = await Trovo.users.get(["InfiniteHorror"]);
-//         const response = await Trovo.channel.edit(channel.users[0].user_id);
-//         expect(response.empty).toBe("");
-//     });
+describe("Channel", () => {
+    // test("Edit Channel", async () => {
+    //     const channel = await Trovo.users.get(["InfiniteHorror"]);
+    //     const response = await Trovo.channel.edit(channel.users[0].user_id);
+    //     expect(response.empty).toBe("");
+    // });
 
-//     test("Get Emotes", async () => {
-//         const emotes = await Trovo.channel.emotes(0, [user_id]);
-//         expect(emotes).toBeTruthy();
-//     });
+    // test("Get Emotes", async () => {
+    //     const emotes = await Trovo.channel.emotes(0, [user_id]);
+    //     expect(emotes).toBeTruthy();
+    // });
 
-//     test("Get Followers", async () => {
-//         const followers = await Trovo.channel.followers(second_id);
-//         expect(followers).toBeTruthy();
-//     });
+    // test("Get Followers", async () => {
+    //     const followers = await Trovo.channel.followers(second_id);
+    //     expect(followers).toBeTruthy();
+    // });
 
-//     test("Get Viewers", async () => {
-//         const channel = await Trovo.users.get(["Wara"]);
-//         const viewers = await Trovo.channel.viewers(channel.users[0].user_id);
-//         expect(viewers).toBeTruthy();
-//     });
+    // test("Get Viewers", async () => {
+    //     const channel = await Trovo.users.get(["Wara"]);
+    //     const viewers = await Trovo.channel.viewers(channel.users[0].user_id);
+    //     expect(viewers).toBeTruthy();
+    // });
 
-//     test("Get Subscriptions", async () => {
-//         const channel = await Trovo.users.get(["HerouTV"]);
-//         const subs = await Trovo.channel.subscribers(channel.users[0].user_id);
-//         expect(subs).toBeTruthy();
-//     });
+    // test("Get Subscriptions", async () => {
+    //     const channel = await Trovo.users.get(["HerouTV"]);
+    //     const subs = await Trovo.channel.subscribers(channel.users[0].user_id);
+    //     expect(subs).toBeTruthy();
+    // });
 
-//     test("Get Stream M3U8 urls", async () => {
-//         const urls = await Trovo.channel.streamUrls(second_id);
-//         expect(urls).toBeTruthy();
-//     });
+    // test("Get Stream M3U8 urls", async () => {
+    //     const urls = await Trovo.channel.streamUrls(second_id);
+    //     expect(urls).toBeTruthy();
+    // });
     
-//     test("Get Clips Info", async () => {
-//         const clips = await Trovo.channel.clips(second_id);
-//         expect(clips).toBeTruthy();
-//     });
+    // test("Get Clips Info", async () => {
+    //     const clips = await Trovo.channel.clips(second_id);
+    //     expect(clips).toBeTruthy();
+    // });
 
-//     test("Get Past Streams", async () => {
-//         const streams = await Trovo.channel.pastStreams(second_id);
-//         expect(streams).toBeTruthy();
-//     });
-// });
+    // test("Get Past Streams", async () => {
+    //     const streams = await Trovo.channel.pastStreams(second_id);
+    //     expect(streams).toBeTruthy();
+    // });
+});
 
 describe("Chat", () => {
     // test("Get Token", async () => {
