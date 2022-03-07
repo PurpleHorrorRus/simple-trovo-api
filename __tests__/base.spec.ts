2fd4c09f6f79247558cc6f75fe73700346f5484b
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-07 16:03:42 +0800

    Update tests

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -31,14 +31,14 @@ beforeAll(async () => {
     });
 });
 
-// describe("Main", () => {
-//     test("Get Auth Link", () => {
-//         const link = Trovo.getAuthLink([], "https://purplehorrorrus.github.io/token");
-//         expect(link).toBeTruthy();
-//     });
-// });
+describe("Main", () => {
+    // test("Get Auth Link", () => {
+    //     const link = Trovo.getAuthLink([], "https://purplehorrorrus.github.io/token");
+    //     expect(link).toBeTruthy();
+    // });
+});
 
-// describe("Users", () => { 
+describe("Users", () => { 
     // test("Get User", async () => {
     //     const user = await Trovo.users.getUserInfo();
     //     expect(user).toBeTruthy();
@@ -46,106 +46,106 @@ beforeAll(async () => {
 
     // test("Get Users by ID", async () => { 
     //     const response = await Trovo.users.get(testingUsers);
-    //     expect(response.length).toBeGreaterThan(0);
+    //     expect(response.users.length).toBeGreaterThan(0);
     // });
-// });
+});
 
-// describe("Categories", () => {
+describe("Categories", () => {
     // test("Get Categories", async () => {
     //     const categories = await Trovo.categories.get();
-    //     expect(categories.length).toBeGreaterThan(0);
+    //     expect(categories.category_info.length).toBeGreaterThan(0);
     // });
 
     // test("Search Categories", async () => {
     //     const result = await Trovo.categories.search("League of");
     //     expect(result).toBeTruthy();
     // });
-// });
+});
 
 // describe("Channels", () => {
-    // test("Get Channel", async () => {
-    //     const channel = await Trovo.channels.get("HerouTV");
-    //     expect(channel).toBeTruthy();
-    // });
+//     test("Get Channel", async () => {
+//         const channel = await Trovo.channels.get("HerouTV");
+//         expect(channel).toBeTruthy();
+//     });
 
-    // test("Top Channels", async () => {
-    //     const top = await Trovo.channels.top();
-    //     expect(top).toBeTruthy();
-    // });
+//     test("Top Channels", async () => {
+//         const top = await Trovo.channels.top();
+//         expect(top).toBeTruthy();
+//     });
 // });
 
 // describe("Channel", () => {
-    // test("Edit Channel", async () => {
-    //     const channel = await Trovo.users.get(["InfiniteHorror"]);
-    //     const response = await Trovo.channel.edit(channel.users[0].user_id);
-    //     expect(response.empty).toBe("");
-    // });
+//     test("Edit Channel", async () => {
+//         const channel = await Trovo.users.get(["InfiniteHorror"]);
+//         const response = await Trovo.channel.edit(channel.users[0].user_id);
+//         expect(response.empty).toBe("");
+//     });
 
-    // test("Get Emotes", async () => {
-    //     const emotes = await Trovo.channel.emotes(0, [user_id]);
-    //     expect(emotes).toBeTruthy();
-    // });
+//     test("Get Emotes", async () => {
+//         const emotes = await Trovo.channel.emotes(0, [user_id]);
+//         expect(emotes).toBeTruthy();
+//     });
 
-    // test("Get Followers", async () => {
-    //     const followers = await Trovo.channel.followers(second_id);
-    //     expect(followers).toBeTruthy();
-    // });
+//     test("Get Followers", async () => {
+//         const followers = await Trovo.channel.followers(second_id);
+//         expect(followers).toBeTruthy();
+//     });
 
-    // test("Get Viewers", async () => {
-    //     const channel = await Trovo.users.get(["Wara"]);
-    //     const viewers = await Trovo.channel.viewers(channel.users[0].user_id);
-    //     expect(viewers).toBeTruthy();
-    // });
+//     test("Get Viewers", async () => {
+//         const channel = await Trovo.users.get(["Wara"]);
+//         const viewers = await Trovo.channel.viewers(channel.users[0].user_id);
+//         expect(viewers).toBeTruthy();
+//     });
 
-    // test("Get Subscriptions", async () => {
-    //     const channel = await Trovo.users.get(["HerouTV"]);
-    //     const subs = await Trovo.channel.subscribers(channel.users[0].user_id);
-    //     expect(subs).toBeTruthy();
-    // });
+//     test("Get Subscriptions", async () => {
+//         const channel = await Trovo.users.get(["HerouTV"]);
+//         const subs = await Trovo.channel.subscribers(channel.users[0].user_id);
+//         expect(subs).toBeTruthy();
+//     });
 
-    // test("Get Stream M3U8 urls", async () => {
-    //     const urls = await Trovo.channel.streamUrls(second_id);
-    //     expect(urls).toBeTruthy();
-    // });
+//     test("Get Stream M3U8 urls", async () => {
+//         const urls = await Trovo.channel.streamUrls(second_id);
+//         expect(urls).toBeTruthy();
+//     });
     
-    // test("Get Clips Info", async () => {
-    //     const clips = await Trovo.channel.clips(second_id);
-    //     expect(clips).toBeTruthy();
-    // });
+//     test("Get Clips Info", async () => {
+//         const clips = await Trovo.channel.clips(second_id);
+//         expect(clips).toBeTruthy();
+//     });
 
-    // test("Get Past Streams", async () => {
-    //     const streams = await Trovo.channel.pastStreams(second_id);
-    //     expect(streams).toBeTruthy();
-    // });
+//     test("Get Past Streams", async () => {
+//         const streams = await Trovo.channel.pastStreams(second_id);
+//         expect(streams).toBeTruthy();
+//     });
 // });
 
-describe("Chat", () => {
-    // test("Get Token", async () => {
-    //     const token = await Trovo.chat.token();
-    //     expect(token).toBeTruthy();
-    // });
+// describe("Chat", () => {
+//     test("Get Token", async () => {
+//         const token = await Trovo.chat.token();
+//         expect(token).toBeTruthy();
+//     });
     
-    // test("Send Message", async () => {
-    //     const message = await Trovo.chat.send("Sended from simple-trovo-api");
-    //     expect(message).toBeTruthy();
-    // });
+//     test("Send Message", async () => {
+//         const message = await Trovo.chat.send("Sended from simple-trovo-api");
+//         expect(message).toBeTruthy();
+//     });
 
-    // test("Receive message", async () => {
-    //     const message = await new Promise(resolve => {
-    //         Trovo.chat.service.on("message", resolve);
-    //     });
+//     test("Receive message", async () => {
+//         const message = await new Promise(resolve => {
+//             Trovo.chat.service.on("message", resolve);
+//         });
 
-    //     expect(message).toBeTruthy();
-    // });
+//         expect(message).toBeTruthy();
+//     });
 
-    test("Delete Message", async () => {
-        await new Promise(resolve => {
-            Trovo.chat.service.once("message", async message => { 
-                await Trovo.chat.delete(user_id, message.message_id, message.uid);
-                return resolve(true);
-            });
+//     test("Delete Message", async () => {
+//         await new Promise(resolve => {
+//             Trovo.chat.service.once("message", async message => { 
+//                 await Trovo.chat.delete(user_id, message.message_id, message.uid);
+//                 return resolve(true);
+//             });
     
-            Trovo.chat.send("(To delete) Sended from simple-trovo-api, " + Date.now());
-        });
-    });
-});
+//             Trovo.chat.send("(To delete) Sended from simple-trovo-api, " + Date.now());
+//         });
+//     });
+// });
