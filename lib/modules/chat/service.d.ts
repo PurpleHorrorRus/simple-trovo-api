6e0695207621354609a053c061af886d21edcf74
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-09 15:47:41 +0800

    Build

--- lib/modules/chat/service.d.ts
+++ lib/modules/chat/service.d.ts
@@ -1,18 +1,20 @@
 /// <reference types="node" />
 import EventEmitter from "events";
 import WebSocket from "ws";
-import { WSMesageData } from "../../interfaces/chat";
+import { ChatServiceConfig, WSMesageData } from "../../interfaces/chat";
 declare class ChatService extends EventEmitter {
     private endpoint;
     private connected;
     private heartbeatRate;
+    private defaultChatServiceConfig;
     private nonces;
+    config: ChatServiceConfig;
     socket: WebSocket;
     heartbeat: NodeJS.Timer;
     private lastMessageTime;
     constructor();
-    connect(token: string): Promise<boolean | void>;
-    disconnect(error: Error | WebSocket.ErrorEvent | unknown): void;
+    connect(token: string, chatServiceConfig?: ChatServiceConfig): Promise<boolean> | boolean;
+    disconnect(error: Error | WebSocket.ErrorEvent | unknown): boolean;
     send(type: string, nonce: string, data?: WSMesageData): boolean;
     messageHandler(message: WebSocket.MessageEvent): any;
     updateTime(): number;
