6d84ded5f87db0a79b65c76407ca9dcf91687fbe
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-10 20:20:17 +0800

    Update declaration file

--- types/lib/requests.d.ts
+++ types/lib/requests.d.ts
@@ -1,14 +1,11 @@
 import { Headers, RequestInit } from "node-fetch";
 import Static from "./static";
 import { TrovoRequestType } from "./types/primary";
-declare type TrovoRequestInit = RequestInit & {
-    ignoreStatus?: boolean;
-};
 declare class TrovoRequests extends Static {
     private headers;
     private apiRoot;
     constructor(headers: Headers);
     private request;
-    requestEndpoint(endpoint: string, params?: TrovoRequestInit): TrovoRequestType;
+    requestEndpoint(endpoint: string, params?: RequestInit): TrovoRequestType;
 }
 export default TrovoRequests;
