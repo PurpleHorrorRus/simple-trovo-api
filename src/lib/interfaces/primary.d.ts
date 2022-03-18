887ab0b9133aa8d0206c1b0e606fec220acd1c14
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-18 23:36:54 +0800

    Remove chat service config from init type

--- src/lib/interfaces/primary.d.ts
+++ src/lib/interfaces/primary.d.ts
@@ -1,11 +1,8 @@
-import { ChatServiceConfig } from "./chat"
-
 export interface TrovoConfig {
     client_id: string
     client_secret: string
     redirect_uri: string
     credits?: string
-    chatServiceConfig?: ChatServiceConfig
 }
 
 export interface AuthParams {
